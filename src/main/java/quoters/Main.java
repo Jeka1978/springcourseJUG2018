a30f74a107dcce878347925520b861cdb2974f5e
bsevgeny@gmail.com <papakita2009>
2018-10-17 17:17:50 +0300

    spring lab1 done

---
+++ src/main/java/quoters/Main.java
@@ -0,0 +1,13 @@
+package quoters;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBeansOfType(Quoter.class).values().forEach(Quoter::sayQuote);
+    }
+}
--- src/main/java/quoters/ShakespearQuoter.java
+++ src/main/java/quoters/ShakespearQuoter.java
@@ -1,8 +1,11 @@
 package quoters;
 
+import lombok.Setter;
+
 /**
  * @author Evgeny Borisov
  */
+@Setter
 public class ShakespearQuoter implements Quoter {
     private String message;
     @Override
